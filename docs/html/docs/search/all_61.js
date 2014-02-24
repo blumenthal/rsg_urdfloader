f1b2d084728988cc1216895a39827d3a8c263927
Praveen Ramanujam <praveen.ramanujam@yahoo.com>
2014-02-24 13:54:28 +0100

    Docu changes

--- docs/html/docs/search/all_61.js
+++ docs/html/docs/search/all_61.js
@@ -3,5 +3,5 @@ var searchData=
   ['addchildrentorsg',['addChildrenToRSG',['../classrsg__urdfloader_1_1URDFtoRSG.html#a171cee8009f284a47e07a7ad1110ba9a',1,'rsg_urdfloader::URDFtoRSG']]],
   ['addgeometry',['addGeometry',['../classrsg__urdfloader_1_1URDFtoRSG.html#aca11ac5f1162319b199e3744cff1194e',1,'rsg_urdfloader::URDFtoRSG']]],
   ['addjoint',['addJoint',['../classrsg__urdfloader_1_1URDFtoRSG.html#a29ebacf54db9f8abbd6126ad860e2315',1,'rsg_urdfloader::URDFtoRSG']]],
-  ['addmassproperties',['addMassProperties',['../classrsg__urdfloader_1_1URDFtoRSG.html#ac45d09b43a73cc5fb794061883ed92ad',1,'rsg_urdfloader::URDFtoRSG']]]
+  ['addmassproperties',['addMassProperties',['../classrsg__urdfloader_1_1URDFtoRSG.html#ac15b33dd2aba57a175f0ec973d23eac8',1,'rsg_urdfloader::URDFtoRSG']]]
 ];
